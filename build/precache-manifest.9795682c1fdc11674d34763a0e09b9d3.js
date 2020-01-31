a3124a88e55462341695492d6d6ffc7eba7991bc
Reggie Morgan <r.morgan.pro@gmail.com>
2020-01-31 18:54:12 +0000

    build app

---
+++ build/precache-manifest.9795682c1fdc11674d34763a0e09b9d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3867c622a40504b3f51d9c4c960a3dde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ed88a01596e5ac2f4c2",
+    "url": "/static/css/main.09df135e.chunk.css"
+  },
+  {
+    "revision": "89ac345cdbcb0c1d77f5",
+    "url": "/static/js/2.6b23d95d.chunk.js"
+  },
+  {
+    "revision": "9caef2de8558d9eb3a1751fabf0968a4",
+    "url": "/static/js/2.6b23d95d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ed88a01596e5ac2f4c2",
+    "url": "/static/js/main.0745cfe1.chunk.js"
+  },
+  {
+    "revision": "dfc1003032967b96b7d2",
+    "url": "/static/js/runtime-main.be79d512.js"
+  }
+]);
