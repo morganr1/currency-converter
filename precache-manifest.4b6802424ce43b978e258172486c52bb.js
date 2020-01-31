bfb2e4e0cfb2230f4e33ec3fa6efe64dce9663bd
Reggie Morgan <r.morgan.pro@gmail.com>
2020-01-31 19:10:40 +0000

    Updates

---
+++ precache-manifest.4b6802424ce43b978e258172486c52bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2805e7b859720d74ae93686689b1d675",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "3c6d3488ff4832fd03f3",
+    "url": "/currency-converter/static/css/main.09df135e.chunk.css"
+  },
+  {
+    "revision": "89ac345cdbcb0c1d77f5",
+    "url": "/currency-converter/static/js/2.6b23d95d.chunk.js"
+  },
+  {
+    "revision": "9caef2de8558d9eb3a1751fabf0968a4",
+    "url": "/currency-converter/static/js/2.6b23d95d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c6d3488ff4832fd03f3",
+    "url": "/currency-converter/static/js/main.fb3dd401.chunk.js"
+  },
+  {
+    "revision": "9ff85c9c509478e33f06",
+    "url": "/currency-converter/static/js/runtime-main.f93e4cc6.js"
+  }
+]);
